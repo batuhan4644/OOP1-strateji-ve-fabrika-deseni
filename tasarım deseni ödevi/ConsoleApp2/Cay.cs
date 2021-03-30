f32b0c6c769982ff95a121f57339cd3978db1f7c
batuhan4644 <batuhan4644@gmail.com>
2021-03-31 02:19:50 +0300

    proje

---
+++ tasarım deseni ödevi/ConsoleApp2/Cay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+	class Cay : Icecek
+	{
+		public override string goruntule()
+		{
+			return base.goruntule();
+		}
+
+	}
+}
---
+++ tasarım deseni ödevi/ConsoleApp2/SicakİcecekStore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+	class SicakİcecekStore:Icecekfabrikasi
+	{
+		public override string createicecek()
+		{
+			return base.createicecek();
+		}
+	}
+}
